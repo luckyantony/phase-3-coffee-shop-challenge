5bb3df43de0f778d1160127c2df1a7ab26d89684
luckyantony <luckyantonyofficial@gmail.com>
2025-05-18 13:58:32 +0300

    Creating instances

---
+++ coffee.py
@@ -0,0 +1,7 @@
+class coffee:
+    def __init__(self, name):
+        if isinstance(name, str) and len(name) >=3:
+            self.name = name
+        
+        else:
+            return ValueError("Name must be a string of atleast 3 characters.")

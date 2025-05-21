ba0ea76ca8d4bb6289580f677fa368c7185984f1
luckyantony <luckyantonyofficial@gmail.com>
2025-05-22 01:51:34 +0300

    Modifying the python test path

--- tests/order_test.py
+++ tests/order_test.py
@@ -1,4 +1,8 @@
-import pytest
+# import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 from customer import Customer
 from coffee import Coffee

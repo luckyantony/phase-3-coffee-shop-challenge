5d97c314c08cb382a6c5ff43a3160784ca791987
luckyantony <luckyantonyofficial@gmail.com>
2025-05-22 01:35:36 +0300

    Added customer tests

---
+++ tests/customer_test.py
@@ -0,0 +1,10 @@
+from customer import Customer
+from coffee import Coffee
+
+def test_customer_name():
+    c = Customer("Alex")
+    assert c.name == "Alex"
+    c.name = "Bob"
+    assert c.name == "Bob"
+    c.name = "X" * 20
+    assert c.name == "Bob"  # Shouldn't change
